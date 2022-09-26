46713ad18da357c4251622bde6663c393bd279c7
brandon-saenz <programador@grupovalcas.mx>
2022-09-25 17:49:05 -0700

    Estableciendo el doc script inicial

--- aplicacion/vistas/principal.php
+++ aplicacion/vistas/principal.php
@@ -1,11 +1,34 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
+	<base href="<?php echo STASIS; ?>/">
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Puerta Norte</title>
+	<script src="assets/js/script.js" type="text/javascript"></script>
+	<link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+	<link href="assets/css/pages/login/classic/login-4.css" rel="stylesheet" type="text/css" />
+	<link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+	<link href="assets/plugins/custom/prismjs/prismjs.bundle.css" rel="stylesheet" type="text/css" />
+	<link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+	<link href="assets/css/themes/layout/header/base/light.css" rel="stylesheet" type="text/css" />
+	<link href="assets/css/themes/layout/header/menu/light.css" rel="stylesheet" type="text/css" />
+	<link href="assets/css/themes/layout/brand/dark.css" rel="stylesheet" type="text/css" />
+	<link href="assets/css/themes/layout/aside/dark.css" rel="stylesheet" type="text/css" />
+	<link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+
 </head>
 <body>
-	<h1>PUERTA NORTE - RESTAURANTE</h1>
+	<div class="intro">
+
+	</div>
+	<footer>
+
+	</footer>
 </body>
 </html>
