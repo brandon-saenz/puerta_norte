99a31f885fbad9cf6982a88a5d3f2d4545b5d0c2
brandon-saenz <programador@grupovalcas.mx>
2022-10-01 22:22:46 -0700

    Adaptación de credenciales para base de datos

--- aplicacion/modelos/db.php
+++ aplicacion/modelos/db.php
@@ -8,16 +8,17 @@ class DB{
     private $charset;
 
     public function __construct(){
-        $this->host     = 'p3plzcpnl487033';
-        $this->db       = 'rancho_tecate';
-        $this->user     = 'p1d67xcfzkkf';
-        $this->password = "Bo0B13S777.";
         $this->charset  = 'utf8mb4';
-        // $this->host     = 'localhost';
-        // $this->db       = 'rancho_tecate';
-        // $this->user     = 'root';
-        // $this->password = "";
-        // $this->charset  = 'utf8mb4';
+        $this->db       = 'rancho_tecate';
+        if(getenv('HTTP_HOST')=='localhost'){
+            $this->host     = 'localhost';
+            $this->user     = 'root';
+            $this->password = "";
+        }else{
+            $this->host     = 'p3plzcpnl487033';
+            $this->user     = 'p1d67xcfzkkf';
+            $this->password = "Bo0B13S777.";
+        }
     }
 
     function connect(){
